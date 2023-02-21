8f990b02d0d8814f4e0d0abe14559a525756b1e8
Santhusha-Mallawatantri <santhusha@wso2.com>
2023-02-21 14:09:38 +0530

    Changes to the facebook side panel

--- apps/console/src/features/identity-providers/data/identity-provider-templates/templates/facebook/create-wizard-help.tsx
+++ apps/console/src/features/identity-providers/data/identity-provider-templates/templates/facebook/create-wizard-help.tsx
@@ -1,77 +1,38 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable sort-keys */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/typedef */
+/* eslint-disable header/header */
 /**
- * Copyright (c) 2021, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
-
-import { TestableComponentInterface } from "@wso2is/core/models";
 import { Code, CopyInputField, Heading, Message } from "@wso2is/react-components";
-import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
+import { AppState, ConfigReducerStateInterface } from "apps/console/src/features/core";
+import React, { useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { Divider } from "semantic-ui-react";
-import { identityProviderConfig } from "../../../../../../extensions/configs";
-import { ConfigReducerStateInterface } from "../../../../../core/models";
-import { AppState } from "../../../../../core/store";
+import { Button, Progress, Segment, Sidebar } from "semantic-ui-react";
 
-/**
- * Prop types of the component.
- */
-type FacebookIdentityProviderCreateWizardHelpPropsInterface = TestableComponentInterface;
+const FacebookIdentityProviderCreateWizardHelp = () => {
+    const { t } = useTranslation();
+    const [ useNewConnectionsView, setUseNewConnectionsView ] = useState<boolean>(undefined);
+    const config: ConfigReducerStateInterface = useSelector((state: AppState) => state.config);
 
-/**
- * Help content for the Facebook IDP template creation wizard.
- *
- * @param props - Props injected into the component.
- *
- *  @returns React Element
- */
-const FacebookIdentityProviderCreateWizardHelp: FunctionComponent<
-    FacebookIdentityProviderCreateWizardHelpPropsInterface> = (
-        props: FacebookIdentityProviderCreateWizardHelpPropsInterface
-    ): ReactElement => {
-
-        const {
-            [ "data-testid" ]: testId
-        } = props;
-
-        const { t } = useTranslation();
-
-        const config: ConfigReducerStateInterface = useSelector((state: AppState) => state.config);
-
-        const [ useNewConnectionsView, setUseNewConnectionsView ] = useState<boolean>(undefined);
-
-        /**
-         * Checks if the listing view defined in the config is the new connections view.
-         */
-        useEffect(() => {
-
-            if (useNewConnectionsView !== undefined) {
-                return;
-            }
-
-            setUseNewConnectionsView(identityProviderConfig.useNewConnectionsView);
-        }, [ identityProviderConfig ]);
-        
-        return (
-            <div data-testid={ testId }>
-                <Message
+    const CONTENTS = [
+        {
+            id: 0,
+            body: (
+                <><Message
                     type="info"
                     header={
                         t("console:develop.features.authenticationProvider.templates.facebook." +
-                        "wizardHelp.preRequisites.heading")
+                    "wizardHelp.preRequisites.heading")
                     }
                     content={
                         (<>
@@ -79,28 +40,28 @@ const FacebookIdentityProviderCreateWizardHelp: FunctionComponent<
                                 <Trans
                                     i18nKey={
                                         "console:develop.features.authenticationProvider.templates.facebook." +
-                                        "wizardHelp.preRequisites.getCredentials"
+                                    "wizardHelp.preRequisites.getCredentials"
                                     }
                                 >
-                                Before you begin, create an <strong>application</strong> <a
+                            Before you begin, create an <strong>application</strong> <a
                                         href="https://developers.facebook.com/"
                                         target="_blank"
                                         rel="noopener noreferrer">
-                                on Facebook Developer Console
+                            on Facebook Developer Console
                                     </a>, and obtain a <strong>App ID & secret</strong>.
                                 </Trans>
                             </p>
                             <p>
-    
+
                                 <Trans
                                     i18nKey={
                                         "console:develop.features.authenticationProvider.templates.facebook." +
-                                        "wizardHelp.preRequisites.configureSiteURL"
+                                    "wizardHelp.preRequisites.configureSiteURL"
                                     }
                                 >
-                                Use the following as the <strong>Site URL</strong>.
+                            Use the following as the <strong>Site URL</strong>.
                                 </Trans>
-    
+
                                 <CopyInputField
                                     className="copy-input-dark spaced"
                                     value={ config?.deployment?.customServerHost }
@@ -110,53 +71,58 @@ const FacebookIdentityProviderCreateWizardHelp: FunctionComponent<
                                 <Trans
                                     i18nKey={
                                         "console:develop.features.authenticationProvider.templates.facebook." +
-                                        "wizardHelp.preRequisites.configureRedirectURL"
+                                    "wizardHelp.preRequisites.configureRedirectURL"
                                     }
                                 >
-                                Add the following URL as a <strong>Valid OAuth Redirect URI</strong>.
+                            Add the following URL as a <strong>Valid OAuth Redirect URI</strong>.
                                 </Trans>
-    
+
                                 <CopyInputField
                                     className="copy-input-dark spaced"
                                     value={ config?.deployment?.customServerHost + "/commonauth" }
                                 />
-    
+
                                 <a
                                     href="https://developers.facebook.com/docs/development/create-an-app"
                                     target="_blank"
                                     rel="noopener noreferrer">
                                     {
                                         t("console:develop.features.authenticationProvider.templates.facebook" +
-                                        ".wizardHelp.preRequisites.configureOAuthApps")
+                                    ".wizardHelp.preRequisites.configureOAuthApps")
                                     }
                                 </a>
                             </p>
                         </>)
                     }
                 />
+
+                </>
+                    
+            )
+        },
+        {
+            id: 1,
+            title:  t("console:develop.features.authenticationProvider.templates.facebook" +
+            ".wizardHelp.name.heading"),
     
-                <Heading as="h5">
-                    {
-                        t("console:develop.features.authenticationProvider.templates.facebook" +
-                                ".wizardHelp.name.heading")
-                    }
-                </Heading>
-                <p>
-                    {
-                        useNewConnectionsView
-                            ? t("console:develop.features.authenticationProvider.templates.facebook." +
-                                    "wizardHelp.name.connectionDescription")
-                            : t("console:develop.features.authenticationProvider.templates.facebook." +
-                                    "wizardHelp.name.idpDescription")
-                    }
-                </p>
-    
-                <Divider/>
-    
-                <Heading as="h5">
-                    { t("console:develop.features.authenticationProvider." +
-                            "templates.facebook.wizardHelp.clientId.heading") }
-                </Heading>
+            body:(    
+                t("Provide a unique name for the selected identity provider to be easily identifiable.")            
+                // <p>
+                // {
+                //     useNewConnectionsView
+                //         ? t("console:develop.features.authenticationProvider.templates.facebook." +
+                //                 "wizardHelp.name.connectionDescription")
+                //         : t("console:develop.features.authenticationProvider.templates.facebook." +
+                //                 "wizardHelp.name.idpDescription")
+                // }
+                // </p>          
+            )
+        },
+        {
+            id: 2,
+            title: t("console:develop.features.authenticationProvider." +
+            "templates.facebook.wizardHelp.clientId.heading"),
+            body:(
                 <p>
                     <Trans
                         i18nKey={
@@ -167,15 +133,13 @@ const FacebookIdentityProviderCreateWizardHelp: FunctionComponent<
                             Provide the <Code>App ID</Code> obtained from Facebook.
                     </Trans>
                 </p>
-    
-                <Divider/>
-    
-                <Heading as="h5">
-                    {
-                        t("console:develop.features.authenticationProvider.templates.facebook" +
-                                ".wizardHelp.clientSecret.heading")
-                    }
-                </Heading>
+            )
+        },
+        {
+            id: 3,
+            title: t("console:develop.features.authenticationProvider.templates.facebook" +
+            ".wizardHelp.clientSecret.heading"),
+            body: (
                 <p>
                     <Trans
                         i18nKey={
@@ -186,15 +150,82 @@ const FacebookIdentityProviderCreateWizardHelp: FunctionComponent<
                             Provide the <Code>App Secret</Code> obtained from Facebook.
                     </Trans>
                 </p>
-            </div>
-        );
-    };
+            )
+        }
+    ];
+
+    const [ currentContent, setCurrentContent ] = useState(0);
+
+    const handleClickLeft = () => setCurrentContent((c) => (c > 0 ? c - 1 : c));
+    const handleClickRight = () =>
+        setCurrentContent((c) => (c < CONTENTS.length - 1 ? c + 1 : c));
+
+    const isLeftButtonDisabled = currentContent === 0;
+    const isRightButtonDisabled = currentContent === CONTENTS.length - 1;
+
+    const leftButtonColor = isLeftButtonDisabled ? "grey" : "orange";
+    const rightButtonColor = isRightButtonDisabled ? "grey" : "orange";
+
+    const progress = (currentContent / (CONTENTS.length - 1)) * 100;
+
+    return (
+        <Sidebar.Pushable>
+            <Sidebar
+                as={ Segment }
+                animation="overlay"
+                direction="left"
+                visible
+                icon="labeled"
+                vertical
+                className="idp-sidepanel-sidebar"
+            >
+                <div className="idp-sidepanel-content">
+                    { CONTENTS.map(({ id, title, body }) => (
+                        <div key={ id } style={ { display: currentContent === id ? "block" : "none" } }>
+                            <Segment
+                                className="idp-sidepanel-segment">
+                                <h2>{ title }</h2>
+                                <p>{ body }</p>
+                            </Segment>
+                        </div>
+                    )) }
+                </div>
+                <div className="idp-sidepanel-footer">
+                    <Progress
+                        percent={ progress }
+                        progress
+                        className="idp-sidepanel-progress"
+                        color="orange"
+                        size="tiny"
+                    />
+                    <div className="idp-sidepanel-buttons">
+                        <Button
+                            icon="chevron left"
+                            color={ leftButtonColor }
+                            onClick={ handleClickLeft }
+                            className="idp-sidepanel-button"
+                            disabled={ isLeftButtonDisabled }
+                        />
+                        <Button
+                            icon="chevron right"
+                            color={ rightButtonColor }
+                            onClick={ handleClickRight }
+                            className="idp-sidepanel-button"
+                            disabled={ isRightButtonDisabled }
+                        >
+                        </Button>
+                    </div>
+                </div>
+            </Sidebar>
+        </Sidebar.Pushable>
+    );
+};
 
 /**
  * Default props for the component
  */
 FacebookIdentityProviderCreateWizardHelp.defaultProps = {
-    "data-testid": "facebook-idp-create-wizard-help"
+    "data-testid": "google-idp-create-wizard-help"
 };
 
 /**
